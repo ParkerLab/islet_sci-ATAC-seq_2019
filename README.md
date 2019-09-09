92170bd10837d9df409d72aa281a20b161b917f6
Vivek Rai <mail@raivivek.in>
2019-09-08 23:59:38 -0400

    Updated README
    Signed-off-by: Vivek Rai <mail@raivivek.in>

--- README.md
+++ README.md
@@ -17,6 +17,15 @@ Bulk islet samples were processed using the Snakemake pipeline available at
 Scripts that drive the corresponding analysis are contained within the `control`
 directory. Further details are included within each directory.
 
+## Requirements
+
+General requirements for the code contained in this repository is listed below.
+
+* [Snakemake](https://snakemake.readthedocs.io/en/stable/) (5.5.0+)
+* [Slurm](https://slurm.schedmd.com) (16.05.09+) or some other job scheduler.
+* [R](https://www.r-project.org) (3.5.1+). Note that individual script may have different package requirements.
+* [Python](https://www.python.org) (3.7.3+). Specific packages are listed in `requirements.txt`.
+
 ## Questions?
 
 If you see any bug or have any questions, feel free to contact us via GitHub issues or
--- control/mapping/bin/sc_atac_10bpbarcode_split.py
+++ control/mapping/bin/sc_atac_10bpbarcode_split.py
@@ -24,32 +24,32 @@ def submitter(commander):
         submiting.wait()
 
 def editcheck(barc,reflist):
-	try:
-		reflist[barc]
-		eddist = '0'
-	except KeyError:
-		winner = '_CTF' + '_'*(len(barc)-4)
-		winner_ed = 10
-		runnerup_ed = 10
-		for barcode in reflist.keys():
-			curred = Levenshtein.distance(barc,barcode)
-			if curred <= winner_ed:
-				runnerup_ed = winner_ed
-				winner = barcode
-				winner_ed = curred
-		if winner_ed > 3:
-			winner = '_CTF' + '_'*(len(barc)-4)
-		if runnerup_ed - winner_ed < 2:
-			winner = '_AMBIG' + '_'*(len(barc)-6)
-		barc = winner
-		eddist = str(winner_ed)
-	return(barc,eddist)
+        try:
+                reflist[barc]
+                eddist = '0'
+        except KeyError:
+                winner = '_CTF' + '_'*(len(barc)-4)
+                winner_ed = 10
+                runnerup_ed = 10
+                for barcode in reflist.keys():
+                        curred = Levenshtein.distance(barc,barcode)
+                        if curred <= winner_ed:
+                                runnerup_ed = winner_ed
+                                winner = barcode
+                                winner_ed = curred
+                if winner_ed > 3:
+                        winner = '_CTF' + '_'*(len(barc)-4)
+                if runnerup_ed - winner_ed < 2:
+                        winner = '_AMBIG' + '_'*(len(barc)-6)
+                barc = winner
+                eddist = str(winner_ed)
+        return(barc,eddist)
 
 complements = {'A':'T', 'T':'A', 'G':'C', 'C':'G','N':'N'}
 def reverse_complement(x):
-	xrev = x[::-1]
-	xrevcomp = ''.join([complements[z] for z in xrev])
-	return xrevcomp
+        xrev = x[::-1]
+        xrevcomp = ''.join([complements[z] for z in xrev])
+        return xrevcomp
 
 nex_i7 = {"ATTACTCG":"","TCCGGAGA":"","CGCTCATT":"","GAGATTCC":"","ATTCAGAA":"","GAATTCGT":"","CTGAAGCT":"","TAATGCGC":"","CGGCTATG":"","TCCGCGAA":"","TCTCGCGC":"","AGCGATAG":""}
 pcr_i7 = {"TCGGATTCGG":"","GCGGCTGCGG":"","AGATTACGTT":"","CTAACTAGGT":"","CATAGCGACC":"","CCGCTAAGAG":"","ATGGAACGAA":"","GCGTTCCGTT":"","GGTTATCGAA":"","GCATCGTATG":"","AATACGATAA":"","TTCCGTCGAC":"","TCCGGCTTAT":"","ACCAGGCGCA":"","AGAGGAGAAT":"","GTACTCCTAT":"","GCTAACGGAT":"","AGTTGAATCA":"","TGATTAGGTA":"","TCGTAGCATC":"","TCTTGAGGTT":"","AGGTCAGCTT":"","TATTAGACTT":"","CTCAATTAGT":"","TCGCCGCCGG":"","CCGTATGATT":"","AACGCGCAGA":"","CTCGTCGTAG":"","CTAATTGCGA":"","CGCGGCCATA":"","AATATTACTT":"","ATTGGCAGAT":"","ATGGCGCCTG":"","ATAAGGACTC":"","TAGTAAGCCG":"","ATTATGCAAG":"","TTGGCAAGCC":"","TTGATTGGCG":"","GCATATGAGC":"","GAACTCGACT":"","CTAGCCAGCC":"","TGCGACCTCT":"","ATTCTTAGCT":"","TTGATACGAT":"","TATAATAGTT":"","TTGCCGTAGG":"","AGACCATATC":"","TTGGTAAGGA":"","CAGCTAGCGG":"","CTAAGCCTTG":"","CGTTACCGCT":"","GACTGGACCA":"","GCAAGACCGT":"","TCAATCTCCT":"","ATACCTCGAC":"","TAGAGGCGTT":"","TAGGTAACTT":"","TTCGAATATT":"","TGGACGACTA":"","GTAGGCTGCA":"","GTAGGATAAG":"","CGTCGAGCGC":"","ACTATTCATT":"","TTGCTTAGAT":"","CGAATGGAGC":"","CTATATAGCC":"","CTACTAATAA":"","TGGTTGCCGT":"","TCCTCTGCCG":"","GATTCTTGAA":"","GTAGCAGCTA":"","CCTCAGCTCC":"","AAGTAGCTCA":"","TATTGCTGGA":"","CCAGATACGG":"","AACGAATTCG":"","CGCTTATCGT":"","AAGTACGCGA":"","GATCTTCGCA":"","TCTTAGCCTG":"","TTATTGAGGC":"","TTGCGAGCAT":"","GCTTGAAGAG":"","AGTCCGCTGC":"","TAAGTCCTGA":"","AGTTCTCATG":"","CAGACTAAGG":"","TCTATCGCTG":"","GCGCTATGGT":"","CATTATTATT":"","AGCCGTAGTT":"","TGATATTGCG":"","ACGGCGTTAA":"","GGCTTACTCC":"","GCGCGTTCAT":"","GAGCGCGATG":""}
@@ -58,11 +58,11 @@ nex_i5 = {"TATAGCCT":"","ATAGAGGC":"","CCTATCCT":"","GGCTCTGA":"","AGGCGAAG":"",
 
 FULL_BARC = {}
 for i in nex_i7.keys():
-	for j in pcr_i7.keys():
-		for k in pcr_i5.keys():
-			for l in nex_i5.keys():
-				currbarc = i + j + k + l
-				FULL_BARC[currbarc] = ""
+        for j in pcr_i7.keys():
+                for k in pcr_i5.keys():
+                        for l in nex_i5.keys():
+                                currbarc = i + j + k + l
+                                FULL_BARC[currbarc] = ""
 
 totreads = 0
 exactmatch = 0
@@ -80,56 +80,56 @@ infasta2 = io.BufferedReader(if2)
 reads1 = []
 reads2 = []
 for line in infasta1:
-	seqbarc = line.strip('@').split(':')[0]
-	#barcodes = line.strip().split()[1].split(":")[3]
-	#seqbarc = "".join(barcodes.split('+'))
-	if args.nextseq:
-		seqbarca = seqbarc[0:18]
-		seqbarcb = reverse_complement(seqbarc[18:36])
-		seqbarc = seqbarca + seqbarcb
-	read1 = infasta1.next().strip()
-	plus = infasta1.next()
-	qual1 = infasta1.next().strip()
-	dumpbarc = infasta2.readline()
+        seqbarc = line.strip('@').split(':')[0]
+        #barcodes = line.strip().split()[1].split(":")[3]
+        #seqbarc = "".join(barcodes.split('+'))
+        if args.nextseq:
+                seqbarca = seqbarc[0:18]
+                seqbarcb = reverse_complement(seqbarc[18:36])
+                seqbarc = seqbarca + seqbarcb
+        read1 = infasta1.next().strip()
+        plus = infasta1.next()
+        qual1 = infasta1.next().strip()
+        dumpbarc = infasta2.readline()
         read2 = infasta2.readline().strip()
         plus = infasta2.readline()
         qual2 = infasta2.readline().strip()
-	try:
-		FULL_BARC[seqbarc]
-		reads1.append('@' + seqbarc + ':' + str(totreads) + '#0000/1\n' + read1 + '\n+\n' + qual1 + '\n')
-		reads2.append('@' + seqbarc + ':' + str(totreads) + '#0000/2\n' + read2 + '\n+\n' + qual2 + '\n')
-		readcount += 1
-		totreads += 1
-		exactmatch += 1
-	except KeyError:
-		b1 = seqbarc[0:8]
-		b2 = seqbarc[8:18]
-		b3 = seqbarc[18:28]
-		b4 = seqbarc[28:36]
-		b1ed = editcheck(b1,nex_i7)
-		b2ed = editcheck(b2,pcr_i7)
-		b3ed = editcheck(b3,pcr_i5)
-		b4ed = editcheck(b4,nex_i5)
-		seqbarc = b1ed[0] + b2ed[0] + b3ed[0] + b4ed[0]
-		seqed = b1ed[1] + b2ed[1] + b3ed[1] + b4ed[1]
-		reads1.append('@' + seqbarc + ':' + str(totreads) + '#' + seqed + '/1\n' + read1 + '\n+\n' + qual1 + '\n')
+        try:
+                FULL_BARC[seqbarc]
+                reads1.append('@' + seqbarc + ':' + str(totreads) + '#0000/1\n' + read1 + '\n+\n' + qual1 + '\n')
+                reads2.append('@' + seqbarc + ':' + str(totreads) + '#0000/2\n' + read2 + '\n+\n' + qual2 + '\n')
+                readcount += 1
+                totreads += 1
+                exactmatch += 1
+        except KeyError:
+                b1 = seqbarc[0:8]
+                b2 = seqbarc[8:18]
+                b3 = seqbarc[18:28]
+                b4 = seqbarc[28:36]
+                b1ed = editcheck(b1,nex_i7)
+                b2ed = editcheck(b2,pcr_i7)
+                b3ed = editcheck(b3,pcr_i5)
+                b4ed = editcheck(b4,nex_i5)
+                seqbarc = b1ed[0] + b2ed[0] + b3ed[0] + b4ed[0]
+                seqed = b1ed[1] + b2ed[1] + b3ed[1] + b4ed[1]
+                reads1.append('@' + seqbarc + ':' + str(totreads) + '#' + seqed + '/1\n' + read1 + '\n+\n' + qual1 + '\n')
                 reads2.append('@' + seqbarc + ':' + str(totreads) + '#' + seqed + '/2\n' + read2 + '\n+\n' + qual2 + '\n')
                 totreads += 1
-		readcount += 1
-		if 'CTF' in seqbarc or 'AMBIG' in seqbarc:
-                	failed += 1
-		else:
-			editmatch += 1
-	if readcount == 250000:		
-		outfasta1.writelines(reads1)
-		outfasta2.writelines(reads2)
-		readcount = 0
-		reads1 = []
-		reads2 = []
+                readcount += 1
+                if 'CTF' in seqbarc or 'AMBIG' in seqbarc:
+                        failed += 1
+                else:
+                        editmatch += 1
+        if readcount == 250000:         
+                outfasta1.writelines(reads1)
+                outfasta2.writelines(reads2)
+                readcount = 0
+                reads1 = []
+                reads2 = []
 
 if readcount > 0:
-	outfasta1.writelines(reads1)
-	outfasta2.writelines(reads2)
+        outfasta1.writelines(reads1)
+        outfasta2.writelines(reads2)
 
 if1.close()
 if2.close()
